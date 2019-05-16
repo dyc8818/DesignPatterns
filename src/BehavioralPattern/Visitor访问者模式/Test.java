dfb35d875d8c6252f1b755b4b5f2055c2dd3ee6f
dy <387768965@qq.com>
2019-05-16 14:54:25 +0800

    访问者模式

--- src/BehavioralPattern/Visitor访问者模式/Test.java
+++ src/BehavioralPattern/Visitor访问者模式/Test.java
@@ -3,7 +3,7 @@ package BehavioralPattern.Visitor访问者模式;
 public class Test {
     public static void main(String[] args) {
         Computer computer = new Computer();
-        ComputerPartVisitorDisplaying computerPartVisitorDisplaying = new ComputerPartVisitorDisplaying();
+        ComputerPartVisitorStartup computerPartVisitorDisplaying = new ComputerPartVisitorStartup();
         computer.accept(computerPartVisitorDisplaying);
         new Mouse().accept(computerPartVisitorDisplaying);
     }
